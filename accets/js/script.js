8e6fecb1cd5cd04b0b9b8a3eea883037e72bf2ac
Hiroki <sgdaikis_0405@yahoo.co.jp>
2022-07-08 03:16:44 +0900

    last commit

--- accets/js/script.js
+++ accets/js/script.js
@@ -1,8 +1,8 @@
 $(function(){
     $('.slider').slick({
-        autoplay: true,//自動的に動き出すか。初期値はfalse。
-        infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-        autoplaySpeed: 3000,//スライドのスピード。初期値は300。
+        autoplay: true,//自動的に動き出すか。
+        infinite: true,//スライドをループさせるかどうか。
+        autoplaySpeed: 3000,//スライドのスピード。
         slidesToShow: 3,//スライドを画面に3枚見せる
         slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
         responsive:[
@@ -19,27 +19,39 @@ $(function(){
         variableWidth: true,//幅の違う画像の高さを揃えて表示
         dots: true,//下部ドットナビゲーションの表示
     });
-    /*
-    $('a[href^=#]').click(function() {
-        var speed = 500;
-        var href = $(this).attr("href");
-        var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top;
-        $('body,html').animate({scrollTop:position}, speed, 'swing');
-        return false
-    }); 
-    $('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
-        var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-        var pos = $(elmHash).offset().top;	//idの上部の距離を取得
-        $('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-        return false;
-    }); */
+
     $('a[href^="#"]').click(function(){
         var speed = 1000;
         var href= $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
-        var position = target.offset().top;
+        var position = target.offset().top - 80;
         $("html, body").animate({scrollTop:position}, speed, "swing");
         return false;
     });
+
+    $('.burger').click(function(){
+        const burger = document.querySelector(".burger")
+        const nav = document.querySelector(".navi")
+        const navi = document.querySelectorAll(".navi li")
+
+        burger.addEventListener("click", () => {
+            nav.classList.toggle("nav-active")
+
+            //forwards　アニメーションをとどまらせる
+            navi.forEach((link, index) => {
+                if(link.style.animation) {
+                    link.style.animation = "";
+                } else {
+                    link.style.animation = `navLinksFade 0.5s ease forwards ${index/7 + 0.4}s`;
+                }
+            });
+            burger.classList.toggle("toggle");
+        });
+    });
+
+    lightbox.option({
+        'resizeDuration': 300,//時間
+        'wrapAround': true,//写真ループ
+    })
+    
 });
